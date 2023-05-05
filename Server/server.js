29de4edf0f3079a08178e498167f30d4cce0be35
Lucas Blom <51747171+SkateFastEatTrash@users.noreply.github.com>
2023-05-05 10:19:37 +0000

    Main requirements done, Login started

--- Server/server.js
+++ Server/server.js
@@ -1,118 +1,168 @@
 // Defining Constants
-const cors = require('cors');
 const express = require('express')
-const bodyParser = require('body-parser');
 const session = require('express-session')
+const bodyParser = require('body-parser')
 
-//Specifying Port
+const app = express()
 const port = 3000
 
-//Secifiying App
-const app = express()
-
-// --------------------------------------------------------------------------------------//
-
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(session({
+  secret: 'superSecret',
+  resave: false,
+  saveUnititialized: true,
+  cookie: {}
+}))
 
 // Prints Portnumber and URL
 app.listen(port, () => {
   console.log(
-    `\nPort: ${port}\thttp://localhost:${port}\tEnjoy ;)`
-  );
-});
+        `\nPort: ${port}\thttp://localhost:${port}\tEnjoy ;)`
+  )
+})
 
-
-// ToDo List containing all tasks
-var ToDo = [
-    {
-        "id": 1,
-        "task": "Complete math homework",
-        "completed": true
-    },
-    {
-        "id": 2,
-        "task": "Buy groceries",
-        "completed": false
-    },
-    {
-        "id": 3,
-        "task": "Call mom",
-        "completed": false
-    }
+// ToDo List containing all pre existing tasks
+const ToDo = [
+  {
+    id: 1,
+    task: 'Complete math homework',
+    creationDate: '2021-03-01',
+    completionDate: '2021-03-03'
+  },
+  {
+    id: 2,
+    task: 'Buy groceries',
+    creationDate: '2021-03-01',
+    completionDate: '2021-03-03'
+  },
+  {
+    id: 3,
+    task: 'Call mom',
+    creationDate: '2021-03-01',
+    completionDate: '2021-03-03'
+  }
 ]
 
-
-// Functions declaration here
+// --------------------------------function-declaration--------------------------------//
 
 // Function to return all tasks
-function returnAll() {
-  return ToDo;
+function returnAll () {
+  return ToDo
 };
 
 // Function to add a task
-function addTask(id, task, completed) {
-    const newTask = {
-      "id": id,
-      "task": task,
-      "completed": completed,
-    };
-    ToDo.push(newTask);
-  };
+function addTask (id, task, completionDate) {
+  const creationDate = new Date().toISOString().slice(0, 10)
+  const newTask = {
+    id,
+    task,
+    creationDate,
+    completionDate
+  }
+  ToDo.push(newTask)
+};
 
 // Function to get a task by id
-function getTask(id) {
-    const foundTask = ToDo.find(task => task.id == id);
-    return foundTask;
-    };
+function getTask (id) {
+  // eslint-disable-next-line eqeqeq
+  const foundTask = ToDo.find(task => task.id == id)
+  return foundTask
+};
 
 // Function to update a task
-function updateTask(id, task, completed) {
-    const updatedTask = ToDo.find(task => task.id == id);
-    updatedTask.task = task;
-    updatedTask.completed = completed;
-    return updatedTask;
-    };
+function updateTask (id, task, completionDate) {
+  // eslint-disable-next-line eqeqeq
+  const updatedTask = ToDo.find(task => task.id == id)
+  updatedTask.task = task
+  updateTask.completionDate = completionDate
+  return updatedTask
+};
 
 // Function to delete a task
-function deleteTask(id) {
-    const index = ToDo.findIndex(task => task.id == id);
-    ToDo.splice(index, 1);
-    };
+function deleteTask (id) {
+  // eslint-disable-next-line eqeqeq
+  const index = ToDo.findIndex(task => task.id == id)
+  ToDo.splice(index, 1)
+};
 
-//End-Points declared here
+// check if task id exists
+function checkId (id) {
+  // eslint-disable-next-line eqeqeq
+  const foundTask = ToDo.find(task => task.id == id)
+  if (foundTask) {
+    return true
+  } else {
+    return false
+  }
+};
+// --------------------------------main requirements here--------------------------------//
 
 // Get all tasks
 app.get('/tasks', (req, res) => {
-    res.send(returnAll())
-});
+  res.send(returnAll())
+})
 
 // Add a task
 app.post('/tasks', (req, res) => {
-    const id = req.query.id;
-    const task = req.query.task;
-    const completed = req.query.completed;
-    addTask(id, task, completed);
-    res.send(ToDo[ToDo.length - 1]);
-});
+  const id = req.query.id
+  const task = req.query.task
+  const completionDate = req.query.completionDate
+  if (checkId(id)) {
+    res.status(400).json('Task already exists')
+  } else {
+    addTask(id, task, completionDate)
+    res.status(201).send(ToDo[ToDo.length - 1])
+  }
+})
 
 // Get a task by id
-app.get('/task/:id', (req, res) => {
-    const id = req.params.id;
-    res.send(getTask(id));
-});
+app.get('/tasks/:id', (req, res) => {
+  const id = req.params.id
+  if (!checkId(id)) {
+    res.status(404).json('Task not found')
+  }
+  res.status(200).send(getTask(id))
+})
 
-app.put('/task/:id', (req, res) => {
-    const id = req.params.id;
-    const task = req.query.task;
-    const completed = req.query.completed;
-    res.send(updateTask(id, task, completed));
-});
+// Update a task
+app.put('/tasks/:id', (req, res) => {
+  const id = req.params.id
+  const task = req.query.task
+  const completionDate = req.query.completionDate
+  if (!checkId(id)) {
+    res.status(404).json('Task not found')
+  }
+  res.status(201).send(updateTask(id, task, completionDate))
+})
 
-app.delete('/task/:id', (req, res) => {
-    const id = req.params.id;
-    deleteTask(id);
-    res.send(ToDo);
-});
+// Delete a task
+app.delete('/tasks/:id', (req, res) => {
+  const id = req.params.id
+  if (!checkId(id)) {
+    res.status(404).json('Task not found')
+  }
+  res.status(200).send(getTask(id))
+  deleteTask(id)
+})
+
+// ----------------------------------Cookies--------------------------------//
+app.post('/login', (req, res) => {
+  const email = req.query.email.toLowerCase()
+  const password = req.query.password
+  if (email === 'admin' && password === 'm295') {
+    req.session.email = email
+    res.status(200).json({ email: req.session.email })
+  } else {
+    res.status(401).json({ error: 'Invalid credentials' })
+  }
+})
+
+app.get('/verify', (req, res) => {
+  if (req.session.email) {
+    res.status(200).json({ email: req.session.email })
+  } else {
+    res.status(401).json({ error: 'Not logged in' })
+  }
+}
+)
